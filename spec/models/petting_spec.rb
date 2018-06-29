6e9a59a6e07d58f8f8188d8995e9cc7b03355114
Jirles <emily.jirles@gmail.com>
2018-06-29 13:37:46 -0400

    Add petting specs

--- spec/models/petting_spec.rb
+++ spec/models/petting_spec.rb
@@ -5,4 +5,13 @@ describe 'Petting Model' do
     set_variables
   end
 
+  it 'has attributes user_id, dog_id, name, breed, location, pet_rating, and description' do
+    expect(@linda_bandit).to be_valid
+  end
+
+  it 'must have a user_id' do
+    bad_pet = Petting.create(:dog_id => @whitefang.id, :pet_rating => 4.5, :location => "Fullsteam Brewery")
+    expect(bad_pet).to be_invalid
+  end
+
 end
