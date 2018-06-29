8fbf331a3196d0c5d04d584358f79f2b7edd6f97
Jirles <emily.jirles@gmail.com>
2018-06-29 17:44:02 -0400

    Add display_errors tag to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def display_errors
+    if @errors
+      content_tag :div, :class => 'error-messages-field' do
+        @errors.full_messages.map do |msg|
+          concat(content_tag :p, msg)
+        end
+      end
+    end
+  end
+
 end
