b67a73a042b162a342cdd3d4b0a5330b73aad330
Jirles <emily.jirles@gmail.com>
2018-07-05 16:39:17 -0400

    Add basic dog#index functionality

--- spec/features/dog_features_spec.rb
+++ spec/features/dog_features_spec.rb
@@ -5,18 +5,40 @@ RSpec.describe 'Dog Features', :type => :feature do
     set_variables
   end
 
-  it 'has an index page' do
-    visit dogs_path
-    expect(page.status).to eq(200)
-  end
+  describe 'index page' do
+    before do
+      signup_as_bob_belcher
+    end
 
-  it 'has a list of links to a dog profiles' do
-    visit dogs_path
+    it 'has an index page' do
+      visit dogs_path
+      expect(page.status_code).to eq(200)
+    end
 
-    expect(page).to have_link("Bandit")
-    expect(page).to have_content("Bulldog")
-    expect(page).to have_content("Durham, North Carolina")
-    expect(page).to have_content("4.5 stars")
+    it 'has a list of basic information on dog profiles' do
+      visit dogs_path
+
+      expect(page).to have_content("Bandit")
+      expect(page).to have_content("Bulldog")
+      expect(page).to have_content("Durham, North Carolina")
+      expect(page).to have_content("4.5 stars")
+    end
+
+    it 'has links to a dogs profile' do
+      visit dogs_path
+      click_link 'Bandit'
+
+      expect(page.status_code).to eq(200)
+      expect(page.current_path).to eq(dog_path(@bandit))
+    end
+
+    it 'cannot be accessed unless a user is logged in' do
+      click_link "LogOut"
+
+      visit dogs_path
+      expect(page.current_path).to eq(login_path)
+      expect(page).to have_content("Please login or signup to view more content.")
+    end
   end
   # dogs index & show => public - dogs index should have link in navbar
     # dogs index also has a filter option for results ordering => recently update, highest rating
