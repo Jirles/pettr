f7d0b48e7bfed629aadde21775b62359d9cee876
Jirles <emily.jirles@gmail.com>
2018-07-07 09:03:05 -0400

    Begin Omniauth setup

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+  get '/auth/facebook/callback' => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
