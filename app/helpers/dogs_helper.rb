71a52704a34f24252a0f2ed8cbf0258f5f9d9f1f
Jirles <emily.jirles@gmail.com>
2018-07-08 20:04:12 -0400

    Add rating_display and pettings_display helpers for dog show and index pages

--- app/helpers/dogs_helper.rb
+++ app/helpers/dogs_helper.rb
@@ -1,6 +1,10 @@
 module DogsHelper
 
-  def display_dog_rating(dog)
-    "#{dog.user_rating} stars"
+  def dog_show_rating_display
+    Proc.new { |rating| "#{rating} stars" }
+  end
+
+  def dog_pet_by_user_display
+    Proc.new { | petting | "#{petting.name} was pet by #{petting.user.full_name}!" }
   end
 end
