d0ba4c7a83b022c7ab8205f9daaaea1d80fb6bf7
Jirles <emily.jirles@gmail.com>
2018-07-24 10:25:25 -0400

    Add api directory, data controller for internal api calls

---
+++ app/controllers/api/data_controller.rb
@@ -0,0 +1,18 @@
+class Api::DataController < ApplicationController
+    skip_before_action :require_login
+
+    def pettings 
+    end 
+
+    def petting
+    end 
+
+    def dog
+    end 
+
+    def user 
+    end 
+
+    def comments
+    end 
+end 
