4118bc294d92a35866d71ca9447b16ade45e9479
Jirles <emily.jirles@gmail.com>
2018-07-25 10:02:32 -0400

    Begin converting dog index page to accept api data

---
+++ app/assets/javascripts/dogs.js
@@ -0,0 +1,14 @@
+$(function(){
+
+});
+
+class Dog {
+    constructor(name, breed, age, city, rating, bio){
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.city = city;
+        this.rating = rating;
+        this.bio = bio;
+    }
+}
