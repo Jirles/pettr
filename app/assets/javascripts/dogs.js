fb8f65ab616e0e958f9460d1d7a032cec8ba1c3a
Jirles <emily.jirles@gmail.com>
2018-07-25 10:15:52 -0400

    Add formating methods to js dog model class

--- app/assets/javascripts/dogs.js
+++ app/assets/javascripts/dogs.js
@@ -3,12 +3,35 @@ $(function(){
 });
 
 class Dog {
-    constructor(name, breed, age, city, rating, bio){
+    constructor(id, name, breed, age, city, rating, bio){
+        this.id = id;
         this.name = name;
         this.breed = breed;
         this.age = age;
         this.city = city;
         this.rating = rating;
         this.bio = bio;
+    }
+
+    linkToDogProfile(){
+        return `<span class='dog-info-span dog-name-link'><a href='/dogs/${this.id}'>${this.name}</a></span>`;
+    }
+
+    dogInfoSpan(content){
+        return `<span class='dog-info-span'>${content}</span>`;
+    }
+
+    dogRatingDisplay(){
+        return `${this.rating}/5.0 stars!`;
+    }
+
+    createDogProfileCard(){
+        let card = '<div>';
+        card += this.linkToDogProfile();
+        card += this.dogInfoSpan(this.breed);
+        card += this.dogInfoSpan(this.city);
+        card += this.dogInfoSpan(this.dogRatingDisplay());
+        card += '</div>';
+        return card;
     }
 }
